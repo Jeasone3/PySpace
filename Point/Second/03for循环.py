50a214a3a3f41ebb9211e8641330088559db1dd2
Jeason <3390163799@qq.com>
2026-09-11 19:41:14 +0800

    for

--- Point/Second/03for循环.py
+++ Point/Second/03for循环.py
@@ -1,6 +1,9 @@
 
 """
-# range(0,10) 0-10 左闭右开
+range(0,10) 0-10 左闭右开
+# 这个n在for外面定义，可以超出range的值，因为进入for之后n会被初始化为range左边的值
+n = 5
+
 for n in range(10):
     print('你好',n)
 
@@ -14,21 +17,27 @@ for i in nums:
     pass
 """
 
-#加密文件
-text = input('请输入你要加密的文件')
-secret = ''
-for i in text:
-    secret += chr(ord(i) + 1)
-    pass
-print(f'经过加密后的内容为{secret}')
+# 不要在循环内部修改可迭代对象 ,否则会导致死循环  ！！！
+nums = [1,2,3,]
+for i in nums:
+    nums.append(4)
+    print(i)
+
+#
+# 加密文件
+# text = input('请输入你要加密的文件')
+# secret = ''
+# for i in text:
+#     secret += chr(ord(i) + 1)
+#     pass
+# print(f'经过加密后的内容为{secret}')
 
 
 
-secret = input('请输入你要加密的文件')
-for i in secret:
-    text += chr(ord(i)-1)
-    pass
-print(f'经过解密的内容为{text}')
 
-
+# secret = input('请输入你要加密的文件')
+# for i in secret:
+#     text += chr(ord(i)-1)
+#     pass
+# print(f'经过解密的内容为{text}')
 
